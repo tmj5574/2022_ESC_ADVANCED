7af2b42c987a434840686a2ed48ee65d8e38fec7
2021037156 <tmj5574@hanyang.ac.kr>
2022-08-02 17:26:44 +0900

    day2 Mars App

---
+++ codelab/day2/android-basics-kotlin-mars-photos-app-starter/app/src/main/java/com/example/android/marsphotos/overview/OverviewViewModel.kt
@@ -0,0 +1,48 @@
+package com.example.android.marsphotos.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.android.marsphotos.network.MarsApi
+import com.example.android.marsphotos.network.MarsPhoto
+import kotlinx.coroutines.launch
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    private val _status = MutableLiveData<MarsApiStatus>()
+
+    val status: LiveData<MarsApiStatus> = _status//
+
+    private val _photos = MutableLiveData<List<MarsPhoto>>()
+
+    val photos: LiveData<List<MarsPhoto>> = _photos
+    /**
+     * Call getMarsPhotos() on init so we can display status immediately.
+     */
+    init {
+        getMarsPhotos()
+    }
+
+    /**
+     * Gets Mars photos information from the Mars API Retrofit service and updates the
+     * [MarsPhoto] [List] [LiveData].
+     */
+    private fun getMarsPhotos() {
+        viewModelScope.launch {
+            _status.value = MarsApiStatus.LOADING
+            try {
+                _photos.value = MarsApi.retrofitService.getPhotos()
+                _status.value = MarsApiStatus.DONE
+            } catch (e: Exception) {
+                _status.value = MarsApiStatus.ERROR
+                _photos.value = listOf()
+            }
+        }
+    }
+}
